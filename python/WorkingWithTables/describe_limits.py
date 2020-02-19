baed5f74fdc420566abaa78ffb4980f6aa83f1e9
Kirk Kirkconnell <kirkcon@amazon.com>
2020-02-19 12:41:50 -0800

    Update describe_limits.py

--- python/WorkingWithTables/describe_limits.py
+++ python/WorkingWithTables/describe_limits.py
@@ -4,10 +4,7 @@ import boto3, pprint
 # create the resource we need to connect.
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 
-# get the table object
-table = dynamodb.Table("MyTable")  # Substitute your table name for MyTable
-
 # get the table limits from the table object
-response = table.meta.client.describe_limits()
+response = dynamodb.meta.client.describe_limits()
 
 pprint.pprint(response)
