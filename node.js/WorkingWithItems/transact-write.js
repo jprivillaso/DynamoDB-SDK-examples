2015620e3ce044ff4e5edfcd674f8532160390fd
Johan Rin <rin.johan@gmail.com>
2020-05-10 14:52:16 +0200

    refactor code with async/await

--- node.js/WorkingWithItems/transact-write.js
+++ node.js/WorkingWithItems/transact-write.js
@@ -4,47 +4,50 @@ AWS.config.update({ region: "us-west-2" });
 
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
-const params = {
-  TransactItems: [
-    // Update the product status if the condition is met
-    {
-      Update: {
-        TableName: "Products",
-        Key: {
-          ProductId: "B07JP6Z9PJ42",
+async function transactWriteItem() {
+  const params = {
+    TransactItems: [
+      // Update the product status if the condition is met
+      {
+        Update: {
+          TableName: "Products",
+          Key: {
+            ProductId: "B07J1337PJ42",
+          },
+          UpdateExpression: "SET ProductStatus = :new_status",
+          ConditionExpression: "ProductStatus = :expected_status",
+          ExpressionAttributeValues: {
+            ":new_status": "SOLD",
+            ":expected_status": "IN_STOCK",
+          },
+          ReturnValuesOnConditionCheckFailure: "ALL_OLD",
         },
-        UpdateExpression: "SET ProductStatus = :new_status",
-        ConditionExpression: "ProductStatus = :expected_status",
-        ExpressionAttributeValues: {
-          ":new_status": "SOLD",
-          ":expected_status": "IN_STOCK",
-        },
-        ReturnValuesOnConditionCheckFailure: "ALL_OLD",
       },
-    },
-    // Create the order if it doesn't already exist
-    {
-      Put: {
-        TableName: "Orders",
-        Item: {
-          OrderId: "171-3549115-4111337",
-          ProductId: "productKey",
-          OrderStatus: "CONFIRMED",
-          OrderTotal: "100",
+      // Create the order if it doesn't already exist
+      {
+        Put: {
+          TableName: "Orders",
+          Item: {
+            OrderId: "171-3549115-4111337",
+            ProductId: "productKey",
+            OrderStatus: "CONFIRMED",
+            OrderTotal: "100",
+          },
+          ConditionExpression: "attribute_not_exists(OrderId)",
+          ReturnValuesOnConditionCheckFailure: "ALL_OLD",
         },
-        ConditionExpression: "attribute_not_exists(OrderId)",
-        ReturnValuesOnConditionCheckFailure: "ALL_OLD",
       },
-    },
-  ],
-  ReturnConsumedCapacity: "TOTAL",
-};
+    ],
+    ReturnConsumedCapacity: "TOTAL",
+  };
+
+  const response = await documentClient.transactWrite(params).promise();
+  return response;
+}
 
 // Execute the actions defined as a single all-or-nothing operation
-documentClient.transactWrite(params, (err, data) => {
-  if (err) {
-    console.error(JSON.stringify(err, null, 2));
-  } else {
-    console.log("transactWriteItem succeeded:", JSON.stringify(data, null, 2));
-  }
-});
+transactWriteItem()
+  .then((data) =>
+    console.log("TransactWriteItem succeeded:", JSON.stringify(data, null, 2))
+  )
+  .catch((error) => console.error(JSON.stringify(error, null, 2)));
