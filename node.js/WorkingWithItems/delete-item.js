bc8a9e83242dccb76a1894a14cf5b0b7fb754636
Johan Rin <rin.johan@gmail.com>
2020-05-10 14:18:01 +0200

    refactor code with async/await

--- node.js/WorkingWithItems/delete-item.js
+++ node.js/WorkingWithItems/delete-item.js
@@ -5,22 +5,23 @@ AWS.config.update({ region: "us-west-2" });
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
 // Notice the conditional expression where it will only delete if the postal code is the specified value
-const params = {
-  TableName: "RetailDatabase",
-  Key: {
-    pk: "jim.bob",
-    sk: "metadata",
-  },
-  ConditionExpression: "shipaddr.pcode = :val",
-  ExpressionAttributeValues: {
-    ":val": "98260",
-  },
-};
+async function deleteItem() {
+  const params = {
+    TableName: "RetailDatabase",
+    Key: {
+      pk: "jim.bob@somewhere.com",
+      sk: "metadata",
+    },
+    ConditionExpression: "address.pcode = :val",
+    ExpressionAttributeValues: {
+      ":val": "98260",
+    },
+  };
 
-documentClient.delete(params, (err, data) => {
-  if (err) {
-    console.error(JSON.stringify(err, null, 2));
-  } else {
-    console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
-  }
-});
+  const response = await documentClient.delete(params).promise();
+  return response;
+}
+
+deleteItem()
+  .then(() => console.log("DeleteItem succeeded"))
+  .catch((error) => console.error(JSON.stringify(error, null, 2)));
--- node.js/WorkingWithItems/put-item.js
+++ node.js/WorkingWithItems/put-item.js
@@ -5,29 +5,30 @@ AWS.config.update({ region: "us-west-2" });
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
 // Define new user data to insert into the system
-const params = {
-  TableName: "RetailDatabase",
-  Item: {
-    pk: "jim.bob@somewhere.com",
-    sk: "metadata",
-    name: "Jim Bob",
-    first_name: "Jim",
-    last_name: "Bob",
-    address: {
-      road: "456 Nowhere Lane",
-      city: "Langely",
-      state: "WA",
-      pcode: "98260",
-      country: "USA",
+async function putItem() {
+  const params = {
+    TableName: "RetailDatabase",
+    Item: {
+      pk: "jim.bob@somewhere.com",
+      sk: "metadata",
+      name: "Jim Bob",
+      first_name: "Jim",
+      last_name: "Bob",
+      address: {
+        road: "456 Nowhere Lane",
+        city: "Langely",
+        state: "WA",
+        pcode: "98260",
+        country: "USA",
+      },
+      username: "jbob",
     },
-    username: "jbob",
-  },
-};
+  };
 
-documentClient.put(params, (err, data) => {
-  if (err) {
-    console.error(JSON.stringify(err, null, 2));
-  } else {
-    console.log("PutItem succeeded:", JSON.stringify(data, null, 2));
-  }
-});
+  const response = await documentClient.put(params).promise();
+  return response;
+}
+
+putItem()
+  .then(() => console.log("PutItem succeeded"))
+  .catch((error) => console.error(JSON.stringify(error, null, 2)));
