f4464de270109f68c1653e3a5ea4b9f8e510aeb8
Kirk Kirkconnell <kirkcon@amazon.com>
2020-05-12 16:54:57 -0700

    Added new delete_item to show how to throw error when item does not exist.

--- python/WorkingWithItems/delete_item2.py
+++ python/WorkingWithItems/delete_item2.py
@@ -1,11 +1,19 @@
-# Simple delete item call, but with an optional condition expression to only delete if the postal code is a specific value
+# Normally if you just delete an item and that item does not exists, a delete successful message is still returned.
+# What if you want an error to be thrown?
+# Theisexample will delete an item using a conditional delete, but return an error if the item does not exist.
+# It just looks for the existence of the item with this primary key, but that those attributes exist. If the item does
+# exist, it is deleted. If it does not exist, you get "The conditional request failed".
+
+
 from __future__ import print_function  # Python 2/3 compatibility
 import boto3, json
 from botocore.exceptions import ClientError
 
+
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 
 table = dynamodb.Table('RetailDatabase')
+
 
 try:
     response = table.delete_item(
@@ -13,10 +21,7 @@ try:
             'pk': "jim.bob",
             "sk": "metadata"
         },
-        ConditionExpression="shipaddr.pcode = :val",
-        ExpressionAttributeValues= {
-            ":val": "98260"
-        }
+        ConditionExpression="attribute_exists (pk) AND attribute_exists (sk)",
     )
 except ClientError as e:
     if e.response['Error']['Code'] == "ConditionalCheckFailedException":
