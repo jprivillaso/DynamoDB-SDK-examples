df012faf004887ee670ec4df3c5b38bbbeba3a9d
Johan Rin <rin.johan@gmail.com>
2020-05-25 15:48:19 +0200

    fix typo (#14)

--- node.js/WorkingWithQueries/query-return-consumed-capacity.js
+++ node.js/WorkingWithQueries/query-return-consumed-capacity.js
@@ -22,11 +22,12 @@ const query = async () => {
   if (response.LastEvaluatedKey) {
     console.log(
       `Not all items have been retrieved by this query. At least one another request is required to get all available items. The last evaluated key corresponds to ${JSON.stringify(
-        response.LastEvaluetedKey
+        response.LastEvaluatedKey
       )}.`
     );
+  }
 
-  console.log(`Query response: ${JSON.stringify(response)}`);
+  console.log(`Query response: ${JSON.stringify(response, null, 2)}`);
 };
 
 query().catch((error) => console.error(JSON.stringify(error, null, 2)));
