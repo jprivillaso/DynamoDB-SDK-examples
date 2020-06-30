4b47d219d8d8708c106ac88cd4cb47818586b54d
Juan Rivillas <jprivillaso@gmail.com>
2020-06-29 22:10:46 -0300

    Created programs to change between provisioned and on-demand types

---
+++ golang/WorkingWithTables/describeTable/describeTable.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/dynamodb"
+    "fmt"
+)
+
+func getSession() (*session.Session) {
+    sess := session.Must(session.NewSessionWithOptions(session.Options{
+        SharedConfigState: session.SharedConfigEnable,
+        // Provide SDK Config options, such as Region and Endpoint
+        Config: aws.Config{
+            Region: aws.String("us-west-2"),
+	    },
+    }))
+
+    return sess
+}
+
+func describeTable(tableName string) error {
+    dynamoDBClient := dynamodb.New(getSession())
+    response, err := dynamoDBClient.DescribeTable(&dynamodb.DescribeTableInput{
+			TableName: aws.String(tableName),
+		})
+
+    if (err != nil) {
+        return err
+    }
+
+    fmt.Println(response)
+    return nil
+}
+
+func main() {
+    fmt.Println("Describing Table ...")
+
+    tableName := "Music"
+    describeTable(tableName)
+
+    fmt.Println("Finished ...")
+}
